e4bb671a9e6e00246c30a1039c1214a2895e5292
devnezu <nezu.dev@gmail.com>
2025-10-28 20:19:11 -0300

    Atualiza dependências e adiciona configuração para o serviço Gemini no Vite

--- vite.config.js
+++ vite.config.js
@@ -53,6 +53,14 @@ export default defineConfig(({ command }) => ({
             outDir: 'dist-electron'
           }
         }
+      },
+      {
+        entry: 'electron/geminiService.js',
+        vite: {
+          build: {
+            outDir: 'dist-electron'
+          }
+        }
       }
     ]),
     renderer()
