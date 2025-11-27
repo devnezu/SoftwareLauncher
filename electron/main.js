c8efcf4a45204e0b6a28de67ee9e432236086fe5
devnezu <devnezu@users.noreply.github.com>
2025-11-27 19:25:17 -0300

    fix: disable auto-open devtools

--- electron/main.js
+++ electron/main.js
@@ -201,7 +201,6 @@ function createWindow() {
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
-    mainWindow.webContents.openDevTools();
   });
   Menu.setApplicationMenu(null);
 
